a79213ce679e65666747f355d3156cc7643a60f3
leilj <leilj@touzhijia.com>
2017-09-24 22:27:47 +0800

    注解的方式来使用query

---
+++ query-core/src/main/java/wang/uvu/query/EnableDslQuery.java
@@ -0,0 +1,19 @@
+package wang.uvu.query;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import wang.uvu.query.repository.QueryRepositoryFactoryBean;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+@EnableJpaRepositories(repositoryFactoryBeanClass = QueryRepositoryFactoryBean.class)
+public @interface EnableDslQuery {
+
+}
